598c34e6d250df53ef6e1b4559eceb20b3ddab4f
Chris Oyler <christopher.oyler@gmail.com>
2015-05-25 11:49:36 -0700

    retrieving playlists, tracks

--- client/models/user.js
+++ client/models/user.js
@@ -4,16 +4,22 @@ angular.module('findify')
 .factory('User', function($rootScope, $http, nodeUrl){
   function User(){
   }
-  
+  // var spotId = $rootScope.activeUser.id;
   User.getMe = function(){
     return $http.get('https://api.spotify.com/v1/me');
   };
+  // User.getFavs = function(){
+  //     return $http.get('https://api.spotify.com/v1/users/'+ $rootScope.activeUser.id + '/playlists');
+  // };
   User.getFavs = function(){
-    return $http.put('https://api.spotify.com/v1/me/following?type=artist');
+      return $http.get('https://api.spotify.com/v1/me/tracks');
   };
 
   User.initialize = function(){
     return $http.post(nodeUrl + '/users');
+  };
+  User.find = function(id){
+    return $http.get(nodeUrl + '/users/' + id);
   };
 
   return User;
--- client/views/users/profile.js
+++ client/views/users/profile.js
@@ -1,7 +1,15 @@
 angular.module('findify')
 .controller('ProfileCtrl', function(User, $scope){
+//   setTimeout(
+//   User.getFavs()
+//   .then(function(response){
+//     console.log(response, 'hello');
+//   });
+// ){}
+setInterval(function(){
   User.getFavs()
   .then(function(response){
     console.log(response, 'hello');
   });
+}, 10000);
 });
